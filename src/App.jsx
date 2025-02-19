1bdb51a14d5206689ad2bd09eef20a69e9c4247e
Monika-demo1 <monikademo991@gmail.com>
2025-02-19 17:48:25 +0000

    new update-dev5

--- src/App.jsx
+++ src/App.jsx
@@ -16,9 +16,9 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Hello World, Dev3!!!</h1>
+      <h1>Hello World, Dev5!!!</h1>
 
-      <h2>Welcome, Dev3!!!</h2>
+      <h2>Welcome, Dev5!!!</h2>
 
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
